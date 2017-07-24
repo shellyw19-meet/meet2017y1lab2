4bcc18712865768fe51a21a18d370f4907a619c3
shellyw19-meet <shellyw19@meet.mit.edu>
2017-07-24 12:26:06 +0300

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,43 @@
+import turtle
+
+#M
+turtle.goto(0,200)
+turtle.goto(50,0)
+turtle.goto(100,200)
+turtle.goto(100,0)
+turtle.penup() #m ended
+
+#E starts
+
+turtle.goto(210,200)
+turtle.pendown()
+turtle.goto(110,200)
+turtle.goto(110,0)
+turtle.goto(210,0)
+turtle.penup()
+turtle.goto(110,100)
+turtle.pendown()
+turtle.goto(210,100)
+turtle.penup()
+
+#E ended
+
+#second e
+turtle.goto(320,200)
+turtle.pendown()
+turtle.goto(220,200)
+turtle.goto(220,0)
+turtle.goto(320,0)
+turtle.penup()
+turtle.goto(220,100)
+turtle.pendown()
+turtle.goto(320,100)
+turtle.penup()
+
+#secons e ends
+
+#t starts
+turtle.goto(
+
+
+turtle.mainloop()
